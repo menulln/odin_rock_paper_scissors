0a0a51607812bebc4824140ad2958364ac69f302
menulln <39300523+menulln@users.noreply.github.com>
2022-10-31 13:59:22 +0100

    Add function getComputerChoice() in script.js

--- script.js
+++ script.js
@@ -6,3 +6,21 @@ function getPlayerChoice() {
     }
     return playerChoice;
 }
+
+function getComputerChoice() {
+    let random = Math.floor((Math.random() * 3) + 1);
+    switch (random) {
+        case 1:
+            return 'rock';
+            break;
+        case 2:
+            return 'paper';
+            break;
+        case 3:
+            return 'scissors';
+            break;
+        default: 
+            return console.log('Something in function getComputerChoice() went wrong.');
+            break;
+    }
+}
